b25fff0eb41205982d66de7b8c64455f8ad8d94b
Alexey Novickiy <mr.alexn95@mail.ru>
2017-12-21 10:44:10 +0300

    Update vigenere_square.py

--- vigenere_square.py
+++ vigenere_square.py
@@ -1,3 +1,6 @@
+# chr_s - first symbol of alphabet
+# chr_e - last symbol of alphabet
+
 [[chr((j + i - ord(chr_s)) % (ord(chr_e) - ord(chr_s) + 1) + ord(chr_s))
             for j in range(ord(chr_s), ord(chr_e) + 1)]
             for i in range(ord(chr_e) - ord(chr_s) + 1)]
